0cf1cc58d061ce6c99bf78149effe8bbcf7e84cb
neppedboy <neppedboy@gmail.com>
2021-01-05 16:31:52 +0300

    This is 10th anniversary!

--- dates.js
+++ dates.js
@@ -1,3 +1,11 @@
 let datesoftwothousands = ["2006", "2007", "2008", "2009"];
 
-let datesoftwothausandtens = ["2010", "2011", "2012", "2013", "2014", "2015"];
+let datesoftwothausandtens = [
+  "2010",
+  "2011",
+  "2012",
+  "2013",
+  "2014",
+  "2015",
+  "2016",
+];
