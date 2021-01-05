09d61c72cda51601f3428a63d4d4b0d8fdd61d9d
neppedboy <neppedboy@gmail.com>
2021-01-05 16:19:40 +0300

    My eighth birthday! MINECRAFT IS A MY LIFE!

--- dates.js
+++ dates.js
@@ -1,13 +1,3 @@
-let datesoftwothousands = [
-  "2006",
-  "2007",
-  "2008",
-  "2009",
-];
+let datesoftwothousands = ["2006", "2007", "2008", "2009"];
 
-let datesoftwothausandtens = [
-   "2010",
-   "2011",
-   "2012",
-   "2013"
-]
+let datesoftwothausandtens = ["2010", "2011", "2012", "2013", "2014"];
