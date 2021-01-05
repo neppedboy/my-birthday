2bd6b0453c6b2171cf1902b8155c7b6658763390
neppedboy <neppedboy@gmail.com>
2021-01-05 15:53:52 +0300

    My third birthday! I'm just a child

--- dates.js
+++ dates.js
@@ -1,5 +1,6 @@
 let dates = [
   "2006",
   "2007",
-  "2008" 
+  "2008",
+  "2009" // This year my favorite song was released which I loved in 2020. Check this out: https://open.spotify.com/playlist/37i9dQZF1ELVvaOwxKwPrI?si=7VBzdLRPQjOkGdu4j6o0Wg
 ];
