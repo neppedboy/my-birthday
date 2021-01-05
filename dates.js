8afbee668f00f81de07f31ecc91008d9df7bc0e1
neppedboy <neppedboy@gmail.com>
2021-01-05 16:03:55 +0300

    My Fourth birthday! And new big update!

--- dates.js
+++ dates.js
@@ -1,6 +1,18 @@
-let dates = [
+let datesoftwothousands = [
   "2006",
   "2007",
   "2008",
-  "2009" // This year my favorite song was released which I loved in 2020. Check this out: https://open.spotify.com/playlist/37i9dQZF1ELVvaOwxKwPrI?si=7VBzdLRPQjOkGdu4j6o0Wg
+  "2009",
 ];
+
+let datesoftwothausandtens = [
+   "2010"
+]
+
+
+
+
+
+
+
+
